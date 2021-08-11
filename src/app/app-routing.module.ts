5c8708de055152af62b6e5b2a88cbbcd9eba838b
Anna Toth <toth.annelise@gmail.com>
2021-08-11 20:39:27 +0200

    add: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from 'src/components/screens/admin/admin.component';
+import { CartComponent } from 'src/components/screens/cart/cart.component';
+import { CheckoutComponent } from 'src/components/screens/checkout/checkout.component';
+import { ClothesListComponent } from 'src/components/screens/clothes-list/clothes-list.component';
+import { ClothingUpdateComponent } from 'src/components/screens/clothing-update/clothing-update.component';
+import { ClothingComponent } from 'src/components/screens/clothing/clothing.component';
+import { LoginComponent } from 'src/components/screens/login/login.component';
+import { RegisterComponent } from 'src/components/screens/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'clothes', component: ClothesListComponent },
+  { path: 'clothes/:id', component: ClothingComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'admin/:id', component: ClothingUpdateComponent },
+  { path: '**', component: ClothesListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
