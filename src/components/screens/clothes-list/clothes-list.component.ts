bd318176c408afe3b6d0b7cdec28927611cdbe19
Anna Toth <toth.annelise@gmail.com>
2021-09-01 12:15:25 +0200

    feat: clothes list

--- src/components/screens/clothes-list/clothes-list.component.ts
+++ src/components/screens/clothes-list/clothes-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Clothing } from 'src/models/clothing.model';
+import { ApiService } from 'src/services/api/api.service';
 
 @Component({
   selector: 'app-clothes-list',
   templateUrl: './clothes-list.component.html',
-  styleUrls: ['./clothes-list.component.scss']
+  styleUrls: ['./clothes-list.component.scss'],
 })
 export class ClothesListComponent implements OnInit {
+  clothes: Clothing[] = [];
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.clothes = await this.apiService.getClothes();
   }
-
 }
